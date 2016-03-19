26c5a5fe8337549dee03c4284808f920fbeb3962
Andy Johnson <andy@ajces.com>
2016-03-19 13:24:52 -0700

    added reducers for CHANGE_ROUTE, COUNTER_INCREMENT, COUNTER_DECREMENT, COUNTER_RESET, COUNTER_DELETE, and COUNTER_ADD

--- src/reducer.js
+++ src/reducer.js
@@ -1,25 +1,27 @@
 import mori from 'mori';
+import uuid from 'uuid';
 import {createAction, handleAction, handleActions} from 'redux-actions';
 
 export default handleActions({
   CHANGE_ROUTE: (state, action) => ({
-    
+    mori.updateIn(state, 'path', action.payload);
   }),
-
-  /* removed counter component so these aren't necessary
   COUNTER_INCREMENT: (state, action) => ({
-    // TODO: add logic to handle actions
+    mori.upateIn(state, ['counters', action.payload.id], mori.inc);
   }),
   COUNTER_DECREMENT: (state, action) => ({
-
+    mori.upateIn(state, ['counters', action.payload.id], mori.dec);
   }),
   COUNTER_RESET: (state, action) => ({
-
+    mori.upateIn(state, ['counters', action.payload.id], 0);
   }),
   COUNTER_DELETE: (state, action) => ({
-
-  })
-  */
+    let c = mori.get(state, 'counters');
+    mori.dissoc(c, action.payload.id);
+  }),
+  COUNTER_ADD: (state, action) => ({
+    mori.assocIn(state, ['counters', action.payload.id]);
+  }),
 }, mori.hashMap(
   'path', '/',
   'routes', mori.hashMap()
